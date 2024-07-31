9869d74c868a93e6fae23027f1781269be118a4f
SonicBSV <sonicbsv@gmail.com>
2024-07-31 08:57:16 +0300

    Revert "sdm660-common: Use the common device compatibility matrix from QCOM"
    This reverts commit ad36715ab9864d92dc862f528d7ca89fb869c58d.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -163,7 +163,7 @@ LOC_HIDL_VERSION := 4.0
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
-DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
+DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/framework_compatibility_matrix.xml
 
 ODM_MANIFEST_SKUS += NFC
