f43b7161a0cf2885c25e4699590331142c494f96
SonicBSV <sonicbsv@ya.ru>
2024-08-28 22:05:23 +0300

    sdm660-common: update nfc configs and PRODUCT_PACKAGES

--- sdm660.mk
+++ sdm660.mk
@@ -402,8 +402,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.2.vendor \
     com.android.nfc_extras \
-    NfcNci \
-    SecureElement \
     Tag
 
 # OEM Unlock reporting
