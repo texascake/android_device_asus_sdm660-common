f87fc034920b450a9de2c3603c7b5d4e13702f6f
texascake <budalblimbing@gmail.com>
2024-09-23 23:01:21 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,9 @@
 rm -rf kernel/asus/sdm660
-#git clone --depth=1 --recursive https://github.com/Kneba/kernel_asus_sdm660 -b perf+ kernel/asus/sdm660
 git clone --depth=1 --recursive https://github.com/texascake/kernel_asus_sdm660 kernel/asus/sdm660
-#rm -rf kernel/asus/sdm660/KernelSU/userspace
+rm -rf kernel/asus/sdm660/KernelSU/userspace
+rm -rf kernel/asus/sdm660/KernelSU/website
+rm -rf kernel/asus/sdm660/KernelSU/scripts
+rm -rf kernel/asus/sdm660/KernelSU/docs
 
 rm -rf hardware/qcom-caf/msm8998/audio
 rm -rf hardware/qcom-caf/msm8998/display
